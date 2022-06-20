27eb208909a076832708c975fd3238b5593f3cd0
Raquel Feitosa <maria.silva4199@etec.sp.gov.br>
2022-06-20 20:21:42 -0300

    alteração

--- projeto1b/Program.cs
+++ projeto1b/Program.cs
@@ -10,6 +10,16 @@ namespace projeto1b
     {
         static void Main(string[] args)
         {
+            int numero;
+            Pessoa aluno;
+            aluno = new Pessoa();
+            Pessoa professor = new Pessoa();
+            aluno.nome = "Huiguinho";
+            professor.nome = "Prof.Pardal";
+            Console.WriteLine("Aluno:" + aluno.nome);
+            Console.WriteLine("Prof:" + professor.nome);
+            Console.ReadKey();
+
         }
     }
 }
